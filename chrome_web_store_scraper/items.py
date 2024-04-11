dde520451e1a669ddacfcc97dc57f761f4ede78d
XavierZambrano <xavierzamco@gmail.com>
2024-04-11 16:44:35 -0600

    feat: add dataclass item

--- chrome_web_store_scraper/items.py
+++ chrome_web_store_scraper/items.py
@@ -4,9 +4,30 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from dataclasses import dataclass
 
 
-class ChromeWebStoreScraperItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+@dataclass
+class ChromeWebStoreItem:
+    url: str
+    id: str
+    logo: str
+    banner: str
+    website_owner: str
+    featured: bool
+    rating: float
+    rating_count: int
+    category: str
+    subcategory: str
+    users: int
+    images: list[str]
+    overview: str
+    version: str
+    size: str
+    languages: list[str]
+    last_updated: int
+    developer: dict[str, str]
+    # address
+    # website
+    # email
+    # trader
