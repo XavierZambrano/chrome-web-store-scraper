6f1abdd7ae9913927cad9cacb351afe8b67c837b
XavierZambrano <xavierzamco@gmail.com>
2024-04-15 14:23:16 -0600

    feat: robots obey false

--- chrome_web_store_scraper/settings.py
+++ chrome_web_store_scraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "chrome_web_store_scraper.spiders"
 #USER_AGENT = "chrome_web_store_scraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
